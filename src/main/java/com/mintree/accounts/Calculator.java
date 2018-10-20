0a1240164292f3305e4134dd98f5f453936cb06b
Anand Prakash <aptheone@gmail.com>
2018-10-20 10:43:27 +0000

    FOrmating

--- src/main/java/com/mintree/accounts/Calculator.java
+++ src/main/java/com/mintree/accounts/Calculator.java
@@ -8,34 +8,34 @@ package com.mintree.accounts;
  *
  */
 public class Calculator {
-	
+
 	public int add(int a, int b) {
-		
-		if (a<0 && b<0) {
-			return a+b;
+
+		if (a < 0 && b < 0) {
+			return a + b;
 		} else {
-			return a+b;
+			return a + b;
 		}
-		
-		
+
 	}
 
 	public int substract(int a, int b) {
-		if(a>=b) {
-			return a-b;	
-		}else {
-			return b-a;
+		if (a >= b) {
+			return a - b;
+		} else {
+			return b - a;
 		}
-		
+
 	}
+
 	public int multiply(int a, int b) {
-		return a*b;
+		return a * b;
 	}
+
 	public double divide(int a, int b) {
-		if(b!=0) {
-			return a/b;
-		}
-		else {
+		if (b != 0) {
+			return a / b;
+		} else {
 			return 0.0;
 		}
 	}
--- src/test/java/com/mindtree/accounts/test/CalculatorTest.java
+++ src/test/java/com/mindtree/accounts/test/CalculatorTest.java
@@ -9,46 +9,47 @@ import com.mintree.accounts.Calculator;
 
 public class CalculatorTest {
 
-	static Calculator calculator=null;
+	static Calculator calculator = null;
+
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
-		calculator= new Calculator();
+		calculator = new Calculator();
 	}
 
 	@Test
 	public void testAdd() {
-		int result =calculator.add(2, 10);
+		int result = calculator.add(2, 10);
 		assertEquals(12, result);
-		
-		 result =calculator.add(-2, -10);
+
+		result = calculator.add(-2, -10);
 		assertEquals(-12, result);
-		
-	//	fail("Not yet implemented");
+
+		// fail("Not yet implemented");
 	}
 
 	@Test
 	public void testSubstract() {
-		int result =calculator.substract(10, 2);
+		int result = calculator.substract(10, 2);
 		assertEquals(8, result);
-		
-		 result =calculator.substract(2, 10);
+
+		result = calculator.substract(2, 10);
 		assertEquals(8, result);
-		//fail("Not yet implemented");
+		// fail("Not yet implemented");
 	}
 
 	@Test
 	public void testMultiply() {
-		int result =calculator.multiply(2, 10);
+		int result = calculator.multiply(2, 10);
 		assertEquals(20, result);
-		//fail("Not yet implemented");
+		// fail("Not yet implemented");
 	}
 
 	@Test
 	public void testDivide() {
-		double result =calculator.divide(10,5);
+		double result = calculator.divide(10, 5);
 		assertEquals(2, result, 0);
-		
-		//fail("Not yet implemented");
+
+		// fail("Not yet implemented");
 	}
 
 }
